7d8195549cdb009a086959191676eb68da0b7061
Max Lamb <h.max.lamb@gmail.com>
2026-08-28 21:31:19 -0400

    Added feed follows table

---
+++ sql/schema/003_follow_feeds.sql
@@ -0,0 +1,14 @@
+-- +goose up
+CREATE TABLE feed_follows(
+    id int PRIMARY KEY,
+    user_id uuid NOT NULL,
+    feed_id uuid NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_feed FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE,
+    CONSTRAINT unique_user_feed_pair UNIQUE (user_id, feed_id)
+);
+
+-- -goose down
+DROP TABLE feed_follows;
